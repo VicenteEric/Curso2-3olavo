48f15d5775dbf2eb5c74823229726b410d5e354f
Eric Henrique Alves Vicente <106555851+VicenteEric@users.noreply.github.com>
2024-07-24 11:06:33 -0300

    god

--- main.js
+++ main.js
@@ -37,6 +37,23 @@ function geraSenha(){
         senha = senha + letrasMaiusculas[numeroAleatorio];
     }
     campoSenha.value = senha;
+    classificaSenha(alfabeto.lenght);
 }
+
+function classificaSenha(tamanhoAlfabeto){
+    let entropia = tamanhoSenha * Math.log2(alfabeto.lenght);
+    console.log(entropia);
+forcaSenha.classList.remove('fraca','media','forte');
+if (entropia > 11){
+    forcaSenha.classList.add ('forte');
+} else if (entropia > 35 && entropia < 57) {
+    forcaSenha.classlist.add ('media');
+} else if (entropia <= 35){
+    forcaSenha.classlist.add ('fraca');
+
+}
+
+const valorEntropia = document.querySelector('.entropia');
+valorEntropia.textContent= 2 **Math.floor(entropia)/(100e6*60*60*24);
 
 
